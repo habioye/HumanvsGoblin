b99f9bfab79f7175f5731d9cadc02d4530ee265a
habioye <78454514+habioye@users.noreply.github.com>
2024-05-29 10:42:27 -0400

    added the paint component for drawing the grid

--- HumanvsGoblin/src/main/java/Grid.java
+++ HumanvsGoblin/src/main/java/Grid.java
@@ -1,5 +1,25 @@
-public class Grid {
+import java.awt.*;
+import javax.swing.*;
 
-    public void Display();
+public class Grid extends JPanel {
+    public Grid() {
+        setSize(500,500);
+        setVisible(true);
+    }
+
+
+
+    @Override
+    public void paint(Graphics g) {
+        int gridsize = 40;
+        int units = 20;
+     for (int x = gridsize; x <= (gridsize*units); x+=gridsize) {
+         for (int y = gridsize; y <= (gridsize*units); y+=gridsize) {
+             g.drawRect(x,y,gridsize,gridsize);
+         }
+        }
+    }
+
+
 
 }
