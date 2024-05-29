1222400c4c3e2f60e1ea2119f5a1621154a4d761
habioye <78454514+habioye@users.noreply.github.com>
2024-05-29 11:27:11 -0400

    Created a panel for Grid

--- HumanvsGoblin/src/main/java/Grid.java
+++ HumanvsGoblin/src/main/java/Grid.java
@@ -2,17 +2,24 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Grid extends JPanel {
-    public Grid() {
+    int gridsize;
+    int units;
+    public Grid(int gridsize, int units) {
         setSize(500,500);
         setVisible(true);
+        this.gridsize = gridsize;
+        this.units = units;
+
     }
 
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(gridsize*units+100,gridsize*units+100);
+    }
 
 
     @Override
     public void paint(Graphics g) {
-        int gridsize = 40;
-        int units = 20;
      for (int x = gridsize; x <= (gridsize*units); x+=gridsize) {
          for (int y = gridsize; y <= (gridsize*units); y+=gridsize) {
              g.drawRect(x,y,gridsize,gridsize);
