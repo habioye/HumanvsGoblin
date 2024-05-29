af3230998d74613517b12711c7cad7c50b1fd2a6
habioye <78454514+habioye@users.noreply.github.com>
2024-05-29 10:42:47 -0400

    drew the Grid with the window

--- HumanvsGoblin/src/main/java/HumanvsGoblin.java
+++ HumanvsGoblin/src/main/java/HumanvsGoblin.java
@@ -1,6 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class HumanvsGoblin {
     public static void main(String[] args) {
 
+        JFrame window = new JFrame("Grid");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(new Grid());
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
 
 
         boolean humanT = true;
