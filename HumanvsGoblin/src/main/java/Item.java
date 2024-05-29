6edc75f74ac81a767e4561db9d3e6badd082b498
elhewazy <ahmed.elhewazy@gmail.com>
2024-05-29 13:37:13 -0500

    item

--- HumanvsGoblin/src/main/java/Item.java
+++ HumanvsGoblin/src/main/java/Item.java
@@ -1,10 +1,56 @@
 public class Item {
-    int hp;
-    int attack;
-    double hit_rate;
-    String name;
+	public enum ItemID {
+		GOLD, SWORD
+	}
 
-    public Item() {
+	private int hp;
+	private int attack;
+	private double hit_rate;
+	private String name;
+	private ItemID itemId;
 
-    }
+	public Item() {
+
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public double getHit_rate() {
+		return hit_rate;
+	}
+
+	public void setHit_rate(double hit_rate) {
+		this.hit_rate = hit_rate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ItemID getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(ItemID itemId) {
+		this.itemId = itemId;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
