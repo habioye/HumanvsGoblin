b2fc3e3e37c773fef571582ca9c0dea46c58e298
habioye <78454514+habioye@users.noreply.github.com>
2024-05-30 12:15:39 -0400

    Added the Asset class to be able to store information about each image

---
+++ HumanvsGoblin/src/main/java/PlayerAsset.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+class PlayerAsset {
+    int x,y;
+    int width, height;
+    String path;
+    Image image;
+    public PlayerAsset(int x, int y, int width, int height, String path) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.path = path;
+
+    }
+
+
+}
