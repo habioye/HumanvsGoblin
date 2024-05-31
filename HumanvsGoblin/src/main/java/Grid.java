229bd194b4cabba5d21bb843e77c38ecdb07e3e7
habioye <78454514+habioye@users.noreply.github.com>
2024-05-31 12:03:07 -0400

    Added all drawn components to the same panel

--- HumanvsGoblin/src/main/java/Grid.java
+++ HumanvsGoblin/src/main/java/Grid.java
@@ -2,20 +2,37 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
 
 public class Grid extends JPanel {
     int gridsize;
     int units;
+    ArrayList<PlayerAsset> players;
+    String path;
+
+//    String path;
     MoveHandler moveHandler;
-    public Grid(int gridsize, int units) {
+    Image image;
+    public Grid(int gridsize, int units, ArrayList<PlayerAsset> players, String path) {
         setSize(900,900);
         setVisible(true);
         this.gridsize = gridsize;
         this.units = units;
         moveHandler = new MoveHandler();
+        this.path = path;
+        loadImage(path);
+        this.players = players;
+
 
         this.addKeyListener(moveHandler);
 
+    }
+
+    // Method to load the image
+    private void loadImage(String imagePath) {
+        ImageIcon imageIcon = new ImageIcon(imagePath);
+        this.image = imageIcon.getImage();
     }
 
     @Override
@@ -24,13 +41,25 @@ public class Grid extends JPanel {
     }
 
 
+
     @Override
-    public void paint(Graphics g) {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+//        g.drawImage(image,100,100,gridsize,gridsize,this);
+        loadImage(path);
+
+
      for (int x = gridsize; x <= (gridsize*units); x+=gridsize) {
          for (int y = gridsize; y <= (gridsize*units); y+=gridsize) {
-             g.drawRect(x,y,gridsize,gridsize);
+//             g.drawRect(x,y,gridsize,gridsize);
+             g.drawImage(image,x,y,gridsize,gridsize,this);
          }
         }
+    for (var player: players) {
+        loadImage(player.path);
+        g.drawImage(image,player.x,player.y,player.width,player.height,this);
+
+    }
     }
 
 
