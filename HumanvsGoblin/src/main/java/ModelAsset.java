c2b7e252cea49b13b2766b235b76c0903176707e
habioye <78454514+habioye@users.noreply.github.com>
2024-06-02 18:51:55 -0400

    Changed name of the file to model asset

--- HumanvsGoblin/src/main/java/ModelAsset.java
+++ HumanvsGoblin/src/main/java/ModelAsset.java
@@ -1,12 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
 
-class PlayerAsset {
+class ModelAsset {
     int x,y;
     int width, height;
     String path;
 //    Image image;
-    public PlayerAsset(int x, int y, int width, int height, String path) {
+    public ModelAsset(int x, int y, int width, int height, String path) {
         this.x = x;
         this.y = y;
         this.width = width;
