ed501816dbfcb154ef3a5a61924a5fbe826ef6f2
habioye <78454514+habioye@users.noreply.github.com>
2024-06-03 01:00:18 -0400

    Added music

--- HumanvsGoblin/src/main/java/HumanvsGoblin.java
+++ HumanvsGoblin/src/main/java/HumanvsGoblin.java
@@ -1,15 +1,21 @@
 import java.awt.*;
 import javax.swing.*;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.lang.Thread;
 import java.util.*;
 import java.util.Random;
 
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+
 //package PlayerAsset;
 
-public class HumanvsGoblin{
+public class HumanvsGoblin {
 
     public static Player human = new Player();
     public Player goblin = new Player();
@@ -17,21 +23,36 @@ public class HumanvsGoblin{
     // grid dimensions
     public static int gridWidth = 20;
     public static int gridHeight = 20;
+    public static int cellSize = 40;
 
     // human and goblin paths
-    public static String humanPath = "assets/knight.png";
-    public static String GoblinPath = "assets/goblin.png";
+    public static String humanPath = "assets/runner.gif";
+    public static String stillHumanPath = "assets/knight.png";
+    public static String goblinPath = "assets/goblin.gif";
+    public static String stillGoblinPath = "assets/goblin2.png";
 
+
+    // dungeon music
+    public static String dungeonPath = "assets/Dungeon-Crawler.wav";
+
+    // terrain
+    static String grassPath = "assets/grass.png";
+
+    // Obstacle
+    static String stonePath = "assets/transstone.png";
+    static String treePath = "assets/treepixel.png";
+    static String cratePath = "assets/crate.png";
+
+    // inventory
     public static JFrame inventory = new JFrame();
-//         ImagePanel humanAsset = new ImagePanel(10,10,40,40,"assets/human.jpeg");
-//         ImagePanel goblinAsset = new ImagePanel(100,100,40,40,"assets/goblin.jpeg");
 
-
+    // calculates the conversion of board position to coordinates in JPanel
     public static int playerpos(int p) {
-        return (40 + (p * 40));
+        return (cellSize + (p * cellSize));
     }
 
-    public static int[] randomPos(Set<int[]> arr) {
+    // returns a random unfilled position in the array
+    public static int[] randomPos(int[][] arr) {
         int posx;
         int posy;
         int[] coor = new int[2];
@@ -39,10 +60,10 @@ public class HumanvsGoblin{
         while (true) {
             posx = rand.nextInt(gridWidth);
             posy = rand.nextInt(gridHeight);
-            coor[0] = posx;
-            coor[1] = posy;
-            if (arr.contains(coor)) {
-                arr.remove(coor);
+
+            if (arr[posy][posx] == 0) {
+                coor[0] = posy;
+                coor[1] = posx;
                 break;
             }
 
@@ -50,14 +71,43 @@ public class HumanvsGoblin{
         return coor;
     }
 
+    // Generates a group of random obstacles to be added to the ModelAsset ArrayList
+    public static void addObstacles(int[][] arr, ArrayList<ModelAsset> models, String path, int amount) {
+        int[] coor;
+        for (int i = 0; i < amount; i++) {
+            coor = randomPos(arr);
+            arr[coor[0]][coor[1]] = 1;
+            ModelAsset obstacle = new ModelAsset(playerpos(coor[1]), playerpos(coor[0]), cellSize, cellSize, path);
+            obstacle.obstacle = true;
+            models.add(obstacle);
+        }
+    }
+
+    // Plays music that loops
+    public static void playMusic(String filePath) {
+        try {
+            File musicPath = new File(filePath);
+            if (musicPath.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.loop(clip.LOOP_CONTINUOUSLY);
+                clip.start();
+
+            } else {
+                System.out.println("Can't find file");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error");
+        }
+    }
 
 
     public static void main(String[] args) {
+        // start the music
+        playMusic(dungeonPath);
 
-
-
-
-
+        // adds to backpack
         human.backpackAdd(new sword());
         human.backpackAdd(new sword());
         human.backpackAdd(new sword());
@@ -65,32 +115,33 @@ public class HumanvsGoblin{
         human.backpackAdd(new sword());
         human.backpackAdd(new Gold());
 
-//        String humanPath = "assets/human.png";
 
 
-        // terrain
-        String grassPath = "assets/grass.png";
+        int[][] free = new int[gridHeight][gridWidth];
 
-        // Obstacle
-        String stonePath = "assets/stone.png";
-        String fireHydrantPath = "assets/firehydrant.jpeg";
-        String cratePath = "assets/crate.jpeg";
+        int[] coor;
+        ArrayList<ModelAsset> models = new ArrayList<>();
 
-        Set<int[]> free = new HashSet<int[]>();
-        for (int i  = 0; i < gridHeight; i++) {
-            for (int j  = 0; j < gridWidth; j++) {
-                int[] curr = new int[2];
-                curr[0]= i;
-                curr[1] = j;
-                free.add(curr);
+        // generate obstacles
 
-            }
-        }
-//        Iterator<int[]> it = free.iterator();
-        Random rand = new Random();
-        int[] coor = new int[2];
+        // stone obstacles
+        int stoneCount = 12;
+        addObstacles(free, models, stonePath, stoneCount);
 
+        // tree obstacles
+        int treeCount = 40;
+        addObstacles(free, models, treePath, treeCount);
 
+        // crate obstacles
+        int crateCount = 8;
+        addObstacles(free, models, cratePath, crateCount);
+
+//        for (int i = 0; i < gridWidth; i++) {
+//            for (int j = 0; j < gridWidth; j++) {
+//                System.out.print(free[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
 
         // human position
@@ -105,12 +156,10 @@ public class HumanvsGoblin{
         int gobliny = coor[1];
 
 
-        ModelAsset humanPlayer = new ModelAsset(playerpos(humanx), playerpos(humany), 40, 40, humanPath);
-        ModelAsset goblinPlayer = new ModelAsset(playerpos(goblinx), playerpos(gobliny), 40, 40, goblinPath);
-        ArrayList<ModelAsset> players = new ArrayList<>();
-        players.add(humanPlayer);
-        players.add(goblinPlayer);
-
+        ModelAsset humanPlayer = new ModelAsset(playerpos(humanx), playerpos(humany), cellSize, cellSize, humanPath);
+        ModelAsset goblinPlayer = new ModelAsset(playerpos(goblinx), playerpos(gobliny), cellSize, cellSize, goblinPath);
+        models.add(humanPlayer);
+        models.add(goblinPlayer);
 
 
         JFrame window = new JFrame("Human vs Goblin!");
@@ -121,9 +170,9 @@ public class HumanvsGoblin{
         inventory.setResizable(false);
         inventory.pack();
         inventory.setLocationRelativeTo(null);
-        
-        
-        boolean inventoryFrame = false;
+
+
+//        boolean inventoryFrame = false;
 
 //        PlayerPanel humanAsset = new PlayerPanel(humanPath,40,40,20,20);
 //        PlayerPanel goblinAsset = new PlayerPanel(goblinPath,80,80,20,20);
@@ -133,11 +182,11 @@ public class HumanvsGoblin{
         System.out.println("Stop");
 
 
-        int cellSize = 40;
         int unitLength = gridWidth;
 
-        
-        Grid grid = new Grid(cellSize, unitLength, players, grassPath);
+
+        Grid grid = new Grid(cellSize, unitLength, models, grassPath);
+        grid.setBackground(Color.black);
 
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.add(grid);
@@ -145,25 +194,25 @@ public class HumanvsGoblin{
         grid.setFocusable(true);
         grid.requestFocusInWindow();
 
+        window.setSize(900, 900);
         window.setVisible(true);
-        window.setSize(900,900);
 
         boolean humanT = true;
 
         boolean ch = false;
-        Thread frame = new Thread(()-> {
-           try {
-               while (!grid.moveHandler.getInventory()) {
-                   window.repaint();
-                   Thread.sleep(100);
-               }
-           } catch(Exception e) {
-               System.out.println(e);
-           }
-       }
+        Thread frame = new Thread(() -> {
+            try {
+                while (!grid.moveHandler.getInventory()) {
+                    window.repaint();
+                    Thread.sleep(100);
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
 
-       );
-       frame.start();
+        );
+        frame.start();
         int goblinWait = 200;
         int humanWait = 10;
         while (true) {
@@ -178,23 +227,23 @@ public class HumanvsGoblin{
 
                 if (grid.moveHandler.getLeft()) {
 
-                    if (humanx == 0) continue;
+                    if (humanx == 0 || (free[humany][humanx - 1] == 1)) continue;
                     System.out.println("Left IS MIGHT " + humanx + " " + goblinx);
                     humanx -= 1;
                     ch = true;
                 } else if (grid.moveHandler.getRight()) {
-                    if (humanx == unitLength - 1) continue;
+                    if (humanx == unitLength - 1 || (free[humany][humanx + 1] == 1)) continue;
                     System.out.println("RIGHT IS MIGHT " + humanx + " " + goblinx);
                     humanx += 1;
                     ch = true;
 
                 } else if (grid.moveHandler.getUp()) {
                     System.out.println("right");
-                    if (humany == 0) continue;
+                    if (humany == 0 || (free[humany - 1][humanx] == 1)) continue;
                     ch = true;
                     humany -= 1;
                 } else if (grid.moveHandler.getDown()) {
-                    if (humany == unitLength - 1) continue;
+                    if (humany == unitLength - 1 || (free[humany + 1][humanx] == 1)) continue;
                     humany += 1;
                     ch = true;
                 }
@@ -213,16 +262,22 @@ public class HumanvsGoblin{
                 if (humanx != goblinx) {
                     if (goblinx > humanx) {
                         goblinx -= 1;
+                        if (free[gobliny][goblinx] == 1) goblinx += 1;
+
                     } else {
                         goblinx += 1;
+                        if (free[gobliny][goblinx] == 1) goblinx -= 1;
+
                     }
                 } else if (humany != gobliny) {
 
                     if (gobliny > humany) {
                         gobliny -= 1;
+                        if (free[gobliny][goblinx] == 1) gobliny += 1;
 
                     } else {
                         gobliny += 1;
+                        if (free[gobliny][goblinx] == 1) gobliny -= 1;
                     }
 
                     System.out.println(goblinx + gobliny);
@@ -245,20 +300,16 @@ public class HumanvsGoblin{
                 while (true) {
                     try {
                         Thread.sleep(checkInterval);
-                        if (combat.gobby.isDead()) {
-                            System.out.println("You have killed the goblin");
-                         break;
-                        }
-                        if (combat.human.isDead()) {
-                            System.out.println("You have been killed");
-                        }
+                        if (combat.gobby.isDead() || combat.human.isDead())
+                            break;
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     }
                 }
                 if (combat.gobby.isDead()) {
-                    goblinx = 4;
-                    gobliny = 5;
+                    coor = randomPos(free);
+                    gobliny = coor[0];
+                    goblinx = coor[1];
                 }
                 if (combat.human.isDead()) {
                     System.out.println("You have been killed");
@@ -268,7 +319,6 @@ public class HumanvsGoblin{
                 window.setVisible(true);
 
 
-
             }
             System.out.println("moved");
             humanPlayer.x = playerpos(humanx);
@@ -276,7 +326,7 @@ public class HumanvsGoblin{
             goblinPlayer.x = playerpos(goblinx);
             goblinPlayer.y = playerpos(gobliny);
             grid.repaint();
-                 humanT = !humanT;
-             }
+            humanT = !humanT;
+        }
     }
 }
